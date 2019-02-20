51f96c929d3a1f4199307f47be9fa0f9b6f59150
kor1k <koortttt@gmail.com>
2019-02-20 22:06:24 +0200

    added TopMenu/index.js (still broken)

--- src/components/TopMenu/index.js
+++ src/components/TopMenu/index.js
@@ -1,5 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export const TopMenu = () => {
-
+    return (
+        <div className="col-12 top-menu">
+            <ul>
+                <li>Меню 1</li>
+                <li>Меню 2</li>
+                <li>Меню 3</li>
+                <li>Меню 4</li>
+            </ul>
+        </div>
+    )
 }
